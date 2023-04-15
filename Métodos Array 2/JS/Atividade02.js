ccfaa1365164f865ebf6a42de0cb75407668e93d
Pedro Paulo Ramalho de Almeida <108875939+P3dr0p@users.noreply.github.com>
2023-04-15 11:24:07 -0300

    Add files via upload

---
+++ Métodos Array 2/JS/Atividade02.js
@@ -0,0 +1,7 @@
+// Escreva um script que adiciona um novo elemento no início do array
+
+const jogos = ["Aventura", "Ação", "RPG", "Luta"];
+
+jogos.unshift("Corrida");
+
+console.log(jogos); 
---
+++ Métodos Array 2/JS/Atividade03.js
@@ -0,0 +1,7 @@
+// Escreva um script que adiciona um elemento no final do array
+
+const classes = ["S", "A", "B", "C", "D", "E"];
+
+classes.push("F");
+
+console.log(classes);
