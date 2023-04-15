29fbfff791007eb56a82c7edfa4f37a8390dde60
Pedro Paulo Ramalho de Almeida <108875939+P3dr0p@users.noreply.github.com>
2023-04-15 11:22:39 -0300

    Add files via upload

---
+++ Métodos Array 2/JS/Atividade01.js
@@ -0,0 +1,7 @@
+// Escreva um script que remova o último elemento de um array
+
+const planet = ["Mercúrio", "Vênus", "Marte", "Júpiter", "Saturno", "Urano", "Netuno", "Plutão"]
+
+planet.pop();
+
+console.log(planet);
